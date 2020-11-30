4746cff893530663e9fe6b2527f233621fcf98e6
Viktor Kobykhno <ViktkobST@gmail.com>
2020-11-30 13:26:54 +0900

    Initial Commit

---
+++ testTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by Виктор Кобыхно on 11/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
